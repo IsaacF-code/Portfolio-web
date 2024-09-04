319cb6e1c9806d7a1157f3f41b78b2c3499d6ef4
Isaac Freires Santos <isaacfreires@gmail.com>
2024-09-04 17:28:49 -0300

    🐛 bug: Corrigido erro classList null no console log

--- script.js
+++ script.js
@@ -13,8 +13,14 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navlinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
             })
+            
+            let activeLink = document.querySelector('header nav a [href="#' + id + '"]');
+            if(activeLink) {
+                activeLink.classList.add('active');
+            } else {
+                console.log("Classe .active não funcionou.");
+            }
         }
     })
 }
